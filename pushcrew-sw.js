8f9066aadb2e41e60a37e82e728f172a34b9db5f
AbhijeetWingify <47381082+AbhijeetWingify@users.noreply.github.com>
2020-06-09 01:49:46 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,23 @@
-var version = 10;
-importScripts('https://cdn.pushcrew.com/sw/67b0429be02ca5e26453ea995c008e19.js');
+let pushcrewScript = '';
+
+const Engage = () => {
+  if (typeof window !== 'undefined') {
+    const lhostname = window.location.hostname;
+
+    if (lhostname === 'www.seeking.com' || lhostname === 'staging.seeking.com') {
+      pushcrewScript = 'https://cdn.pushcrew.com/sw/083ee782d50bc0ccef46956552b877a6.js';
+    } else {
+      pushcrewScript = 'https://cdn.pushcrew.com/sw/67b0429be02ca5e26453ea995c008e19.js';
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    const version = 6;
+    importScripts(pushcrewScript);
+  }
+};
+
+const pushcrewInterval = setInterval(Engage, 300);
+
+if (pushcrewScript !== '') {
+  clearInterval(pushcrewInterval);
+}
